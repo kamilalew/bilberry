8f578e6a1a1d839476ad7766eeeb5b97cca230f2
Kamila Lew <kamilalew@Kamilas-MacBook-Pro.local>
2020-08-26 20:19:18 +0200

    Update on scripts to enable nodemon

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Is nodemon working? Is it working now? Yeees')
 })
 
 app.listen(port, () => {
