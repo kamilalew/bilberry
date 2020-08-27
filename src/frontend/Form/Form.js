9bda3c58c6f401fa2118cdc059fd9a1138303ec8
Kamila Lew <kamilalew@Kamilas-MacBook-Pro.local>
2020-08-27 17:31:55 +0200

    State in the form gets updated as new record is entered

--- src/frontend/Form/Form.js
+++ src/frontend/Form/Form.js
@@ -15,7 +15,7 @@ export default class Form extends React.Component {
     }
 
     changeHandler = (e) => {
-        this.setState({[e.target.id]: e.target.value})
+        this.setState({[e.target.name]: e.target.value})
     }
 
     submitHandler = (e) => {
@@ -27,19 +27,22 @@ export default class Form extends React.Component {
         const { id, name, language, description, initRelease} = this.state;
 
         return (
-            <form onSubmit={this.submitHandler}>
-                <input type='text' name='id' placeholder='id' defaultValue = {id} onChange={this.changeHandler}>
-                </input>
-                <input type='text' name='name' placeholder='name' defaultValue = {name} onChange={this.changeHandler}>
-                </input>
-                <input type='text' name='language' placeholder='language' defaultValue = {language} onChange={this.changeHandler}>
-                </input>
-                <input type='text' name='description' placeholder='description' defaultValue = {description} onChange={this.changeHandler}>
-                </input>
-                <input type='text' name='initRelease' placeholder='initRelease' defaultValue = {initRelease} onChange={this.changeHandler}>
-                </input>
-                <button className='add-button button' type='submit'>Add record</button>
-            </form>
+            <div>
+                <div className='formHeading'>Add new technology</div>
+                <form onSubmit={this.submitHandler}>
+                    <input type='text' name='id' placeholder='id' defaultValue = {id} onChange={this.changeHandler}>
+                    </input>
+                    <input type='text' name='name' placeholder='name' defaultValue = {name} onChange={this.changeHandler}>
+                    </input>
+                    <input type='text' name='language' placeholder='language' defaultValue = {language} onChange={this.changeHandler}>
+                    </input>
+                    <input type='text' name='description' placeholder='description' defaultValue = {description} onChange={this.changeHandler}>
+                    </input>
+                    <input type='text' name='initRelease' placeholder='initRelease' defaultValue = {initRelease} onChange={this.changeHandler}>
+                    </input>
+                    <button className='add-button button' type='submit'>Add record</button>
+                </form>
+            </div>
         )
     }
 }
